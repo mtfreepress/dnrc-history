b70b3f1e5f3bf26f055feab9c5ad5dac2252debe
Automated <actions@users.noreply.github.com>
2024-09-13 19:45:06 +0000

    Latest data: Fri Sep 13 19:45:06 UTC 2024

--- remingtonfirepermanent.js
+++ remingtonfirepermanent.js
@@ -45,7 +45,7 @@
   },
   {
     "attributes": {
-      "OBJECTID": 461,
+      "OBJECTID": 457,
       "IncidentName": "Short Draw",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYCMX-240531",
