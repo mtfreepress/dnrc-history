f70c94e2440892310a67926748c0525ae0c9d4e3
Automated <actions@users.noreply.github.com>
2024-09-17 22:44:59 +0000

    Latest data: Tue Sep 17 22:44:59 UTC 2024

--- remingtonfire.js
+++ remingtonfire.js
@@ -35,7 +35,7 @@
       "IncidentTypeKind": "FI",
       "IrwinID": "2b136641-8c91-4c70-960d-64e05bc390e4",
       "GlobalID": "ed31ba29-87b1-47f3-a5ba-91c8be5d3f5a",
-      "ModifiedOnAge": 4,
+      "ModifiedOnAge": 5,
       "FireDiscoveryAge": 26
     },
     "geometry": {
