af479a8b3634a729700a1b8358f847885c76eb17
Automated <actions@users.noreply.github.com>
2024-09-22 20:07:14 +0000

    Latest data: Sun Sep 22 20:07:14 UTC 2024

--- remingtonfire.js
+++ remingtonfire.js
@@ -1,7 +1,7 @@
 [
   {
     "attributes": {
-      "OBJECTID": 187,
+      "OBJECTID": 186,
       "IncidentName": "Remington",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYSHX-240442",
@@ -45,7 +45,7 @@
   },
   {
     "attributes": {
-      "OBJECTID": 335,
+      "OBJECTID": 334,
       "IncidentName": "Short Draw",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYCMX-240531",
@@ -80,7 +80,7 @@
       "IrwinID": "d5407b54-aaaf-4689-a0ad-5a589db67982",
       "GlobalID": "2ed3b5d8-14b2-40d2-8320-fc0b7fa53479",
       "ModifiedOnAge": 0,
-      "FireDiscoveryAge": 10
+      "FireDiscoveryAge": 11
     },
     "geometry": {
       "x": -105.61704999999995,
