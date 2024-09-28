e384530dcf2ef0593cdb2e7aab628716f8120e37
Automated <actions@users.noreply.github.com>
2024-09-28 15:45:38 +0000

    Latest data: Sat Sep 28 15:45:38 UTC 2024

--- remingtonfire.js
+++ remingtonfire.js
@@ -1,7 +1,7 @@
 [
   {
     "attributes": {
-      "OBJECTID": 146,
+      "OBJECTID": 143,
       "IncidentName": "Remington",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYSHX-240442",
@@ -45,7 +45,7 @@
   },
   {
     "attributes": {
-      "OBJECTID": 258,
+      "OBJECTID": 255,
       "IncidentName": "Short Draw",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYCMX-240531",
