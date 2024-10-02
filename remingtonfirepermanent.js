ab3bb414f804abe944c47d0462345555aa416b0a
Automated <actions@users.noreply.github.com>
2024-10-02 23:27:15 +0000

    Latest data: Wed Oct  2 23:27:15 UTC 2024

--- remingtonfirepermanent.js
+++ remingtonfirepermanent.js
@@ -1,7 +1,7 @@
 [
   {
     "attributes": {
-      "OBJECTID": 151,
+      "OBJECTID": 152,
       "IncidentName": "Remington",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYSHX-240442",
@@ -35,7 +35,7 @@
       "IncidentTypeKind": "FI",
       "IrwinID": "2b136641-8c91-4c70-960d-64e05bc390e4",
       "GlobalID": "ed31ba29-87b1-47f3-a5ba-91c8be5d3f5a",
-      "ModifiedOnAge": 10,
+      "ModifiedOnAge": 11,
       "FireDiscoveryAge": 41
     },
     "geometry": {
@@ -45,7 +45,7 @@
   },
   {
     "attributes": {
-      "OBJECTID": 251,
+      "OBJECTID": 252,
       "IncidentName": "Short Draw",
       "IncidentTypeCategory": "WF",
       "UniqueFireIdentifier": "2024-WYCMX-240531",
